9dc669db1c6e9ae47c2e8494d9285d1756a211bb
Bulygin Aleksey <alex987@bk.ru>
2023-02-24 01:49:56 +0300

    Async remove elements

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const chalk = require("chalk");
-const { addNote, getNotes } = require("./notes.controller.js");
+const path = require("path");
+const { addNote, getNotes, removeNote } = require("./notes.controller.js");
 
 const port = 3100;
 
@@ -9,15 +10,33 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "pages");
 
+app.use(express.static(path.resolve(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
-  res.render("index", { title: "Express App", notes: await getNotes() });
+  res.render("index", {
+    title: "Express App",
+    notes: await getNotes(),
+    created: false,
+  });
 });
 
 app.post("/", async (req, res) => {
   await addNote(req.body.title);
-  res.render("index", { title: "Express App", notes: await getNotes() });
+  res.render("index", {
+    title: "Express App",
+    notes: await getNotes(),
+    created: true,
+  });
+});
+
+app.delete("/:id", async (req, res) => {
+  await removeNote(req.params.id);
+  res.render("index", {
+    title: "Express App",
+    notes: await getNotes(),
+    created: false,
+  });
 });
 
 app.listen(port, () => {
